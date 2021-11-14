883ca9eda5519ee2ff7f6b89cce943a7cc48d832
skymyself <62652944+skymyself@users.noreply.github.com>
2021-11-14 16:59:54 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 #定义变量
-jemalloc-ver="5.2.1"
-openssl-ver="1.1.1l"
-nginx-ver="1.20.1"
-nginx-service="/etc/systemd/system/nginx.service"
+jemalloc_ver="5.2.1"
+openssl_ver="1.1.1l"
+nginx_ver="1.20.1"
+nginx_service="/etc/systemd/system/nginx.service"
 
 #更新系统
 apt-get update
@@ -14,13 +13,13 @@ apt-get upgrade -y
 apt-get install build-essential libpcre3 libpcre3-dev zlib1g-dev libssl-dev wget curl tar unzip cmake git -y
 
 #安装jemalloc
-jemalloc-install() {
+jemalloc_install() {
 cd /etc
-wget -nc --no-check-certificate https://github.com/jemalloc/jemalloc/releases/download/${jemalloc-ver}/jemalloc-${jemalloc-ver}.tar.bz2
+wget -nc --no-check-certificate https://github.com/jemalloc/jemalloc/releases/download/${jemalloc_ver}/jemalloc-${jemalloc_ver}.tar.bz2
 
-tar -xvf jemalloc-${jemalloc-ver}.tar.bz2
+tar -xvf jemalloc-${jemalloc_ver}.tar.bz2
 
-cd jemalloc-${jemalloc-ver}
+cd jemalloc-${jemalloc_ver}
 
 apt-get install autogen autoconf
 
@@ -31,25 +30,25 @@ make install
 ldconfig 
 
 cd /etc 
-rm -rf jemalloc-${jemalloc-ver} jemalloc-${jemalloc-ver}.tar.bz2
+rm -rf jemalloc-${jemalloc_ver} jemalloc-${jemalloc_ver}.tar.bz2
 
 }
 
 #下载openssl
-openssl-download() {
+openssl_download() {
 
 cd /etc
 
-wget -c https://www.openssl.org/source/openssl-${openssl-ver}.tar.gz && tar zxf openssl-${openssl-ver}.tar.gz && rm openssl-${openssl-ver}.tar.gz
+wget -c https://www.openssl.org/source/openssl-${openssl_ver}.tar.gz && tar zxf openssl-${openssl_ver}.tar.gz && rm openssl-${openssl_ver}.tar.gz
 
  }
 
  #安装nginx
-nginx-install() {
+nginx_install() {
 cd /etc/
-wget http://nginx.org/download/nginx-${nginx-ver}.tar.gz&&tar xf nginx-${nginx-ver}.tar.gz
+wget http://nginx.org/download/nginx-${nginx_ver}.tar.gz&&tar xf nginx-${nginx_ver}.tar.gz
 
-cd nginx-${nginx-ver}
+cd nginx-${nginx_ver}
 
 ./configure \
   --prefix=/etc/nginx \
@@ -64,7 +63,7 @@ cd nginx-${nginx-ver}
   --with-http_v2_module \
   --with-cc-opt=-O3 \
   --with-ld-opt=-ljemalloc \
-  --with-openssl=../openssl-${openssl-ver} \
+  --with-openssl=../openssl-${openssl_ver} \
   --with-stream \
   --with-stream_ssl_module \
   --with-stream=dynamic \
@@ -76,7 +75,7 @@ make install
  }
 
  #添加nginx-service系统文件
- nginx-service() {
+ nginx_service() {
     cat >$nginx_service <<EOF
 [Unit]
 Description=The NGINX HTTP and reverse proxy server
