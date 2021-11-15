d8871f41bf31b90f874fd072eb4736e1c4df646e
skymyself <62652944+skymyself@users.noreply.github.com>
2021-11-15 17:53:55 +0800

    Create upgrade-nginx.sh

---
+++ upgrade-nginx.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+#定义变量
+jemalloc_ver="5.2.1"
+openssl_ver="1.1.1l"
+nginx_ver="1.20.1"
+
+#停止nginx
+systemctl stop nginx
+
+#更新系统
+apt-get update
+apt-get upgrade -y
+
+#安装jemalloc
+jemalloc_install() {
+cd /etc
+wget -nc --no-check-certificate https://github.com/jemalloc/jemalloc/releases/download/${jemalloc_ver}/jemalloc-${jemalloc_ver}.tar.bz2
+
+tar -xvf jemalloc-${jemalloc_ver}.tar.bz2
+
+cd jemalloc-${jemalloc_ver}
+
+apt-get install autogen autoconf -y
+
+./autogen.sh
+
+make
+make install
+ldconfig 
+
+cd /etc 
+rm -rf jemalloc-${jemalloc_ver} jemalloc-${jemalloc_ver}.tar.bz2
+
+}
+
+#下载openssl
+openssl_download() {
+
+cd /etc
+
+wget -c https://www.openssl.org/source/openssl-${openssl_ver}.tar.gz && tar zxf openssl-${openssl_ver}.tar.gz && rm openssl-${openssl_ver}.tar.gz
+
+ }
+ 
+#安装nginx
+nginx_install() {
+cd /etc/
+wget http://nginx.org/download/nginx-${nginx_ver}.tar.gz && tar xf nginx-${nginx_ver}.tar.gz
+
+cd nginx-${nginx_ver}
+
+./configure \
+  --prefix=/etc/nginx \
+  --with-http_ssl_module \
+  --with-http_gzip_static_module \
+  --with-http_stub_status_module \
+  --with-pcre \
+  --with-http_realip_module \
+  --with-http_flv_module \
+  --with-http_mp4_module \
+  --with-http_secure_link_module \
+  --with-http_v2_module \
+  --with-cc-opt=-O3 \
+  --with-ld-opt=-ljemalloc \
+  --with-openssl=../openssl-${openssl_ver} \
+  --with-stream \
+  --with-stream_ssl_module \
+  --with-stream=dynamic \
+  --with-stream_ssl_preread_module
+  
+make
+
+#备份旧的nginx二进制文件
+mv /etc/nginx/sbin/nginx /etc/nginx/sbin/nginx.old
+
+#把新版本的nginx二进制文件到原来nginx的路径
+cp /etc/nginx-${nginx_ver}/objs/nginx /etc/nginx/sbin/
+
+#更新版本
+make upgrade
+
+ }
+ 
+
+jemalloc_install
+openssl_download
+nginx_install
+nginx_service
+echo "检查nginx版本与自己安装的版本对不对"
+/etc/nginx/sbin/nginx -V
+echo "检查nginx运行状态"
+systemctl status nginx 
+echo "nginx升级结束"
